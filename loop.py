aa0ba974e2376bec378af378b5dcc70bd4a8fc48
st sabir92 <sabirst46@gmail.com>
2026-03-30 16:22:44 +0545

    Add a new paragraph and python loop

---
+++ loop.py
@@ -0,0 +1,12 @@
+while True:
+    coffee = "situation of coffee"
+    if(coffee == "empty"):
+        print("fill..")
+        break
+    elif(coffee == "full"):
+        print("drink..")
+        break
+    else:
+        print("it's ok...")
+    pass
+print("So Procced...")
